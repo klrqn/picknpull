6c9bf97c32ab74d8c518b8aec1ae13bdf7eeaad0
john doe <scottbruceburton@gmail.com>
2020-09-29 17:27:50 -0400

    ugh

--- main.py
+++ main.py
@@ -2,21 +2,24 @@
 # main.py - looks throuh pick n pull website for cars that i'm looking for.
 # sends email when there is an update.
 
-import requests, bs4, time
-import selenium
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 
-website = 'https://www.picknpull.com/check_inventory.aspx?Zip=02905&Make=234&Model=4350&Year=&Distance=25&Lat=41.784005&Lon=-71.400250'
+WEBSITE = 'https://www.picknpull.com/check_inventory.aspx?Zip=02905&Make=234&Model=4350&Year=&Distance=25&Lat=41.784005&Lon=-71.400250'
 
-res = requests.get(website)
-
+res = requests.get(WEBSITE)
 res.raise_for_status() # make sure response is 200
+
+parse_content(res)
 
 # time.sleep(1)
 
 # cars = [] # list of cars at pick-n-pulls nearby
 
-PNP_html = bs4.BeautifulSoup(res.text, 'html.parser')
-
-
-print(PNP_html.select('.vehicle-year')[0].getText())
-
+# PNP_html = BeautifulSoup(res.text, 'html.parser')
+#
+#
+# print(PNP_html.select('.vehicle-year')[0].getText())
+#
